863e895cbcbf42a3cd331c04b626d69977ece4f5
Andres Letelier <almlogistico@gmail.com>
2023-03-26 14:51:54 -0300

    actualiza formatos de codigo

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,24 +3,15 @@ import { Route, Routes} from 'react-router-dom'
 
 import Landing from '../components/Landing'
 import {Home} from '../components/Home'
-import Error404 from '../components/Error404'
+
 
 export const AppRouter = () => {
   return (
     <>
-    <Routes>          
-            <Route path='*' element={<Error404/>}/>
+    <Routes>  
             <Route path="/home/*" element={<Home/>}/>
             <Route path="/" element={<Landing/>}/>
-    </Routes>
-    {/* <Routes>
-            <Route path="/equipos" element={<Equipos/>}/>
-            <Route path="/entradas" element={<Entradas/>}/>
-            <Route path="/salidas" element={<Salidas/>}/>
-            <Route path="/clientes" element={<Clientes/>}/>
-            <Route path="/proveedores" element={<Proveedores/>}/>
-            <Route path="/usuarios" element={<Usuarios/>}/>
-    </Routes> */}
+    </Routes>    
     </>
     
       )
