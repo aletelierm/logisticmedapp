9a5a85bdef935c10242d6382ba2d353d95fcfdee
Andres Letelier <almlogistico@gmail.com>
2023-04-08 14:39:48 -0400

    actua

--- src/components/AgregarFamilia.js
+++ src/components/AgregarFamilia.js
@@ -1,5 +1,43 @@
 import React, { useState } from 'react'
 import '../styles/agregarFamilia.css'
+import * as FaIcons from 'react-icons/fa';
+
+import styled from 'styled-components';
+
+
+const ContainerFamily = styled.div`
+        width: 600px;
+        font-family: 'Roboto', sans-serif;
+        text-align: center;
+        padding: 40px 80px;
+        border-radius: 10px ;
+        border: 2px solid green;
+        box-shadow:   7px 4px 5px 0px rgba(0,0,0,0.55);;
+        margin-top: 100px;
+        margin-left: 100px;
+        /* background-color: #C7DCC7; */
+     
+`
+const Formulario = styled.form`
+    display: flex;
+    justify-content: space-around ;
+    gap: 20px;
+    margin: 30px 0 0 0;
+    padding-left: 150px;
+    
+`
+const Boton = styled.form`
+    margin-top: 15px;
+    font-size: 40px;
+	color: #80BE4E;
+	cursor: pointer;
+  
+
+`
+const Etiqueta = styled.input`
+        border-color: green;
+`
+
 
 const AgregarFamilia = () => {
 
@@ -14,13 +52,13 @@ const AgregarFamilia = () => {
     }
 
     return (
-        <div className='containerFamily'>
+        <ContainerFamily>
             <h2 className='titleForm'>Equipamiento Médico</h2>
             <div>
-                <form action='' className='formulario'>
+                <Formulario action='' className='formulario'>
                     <div>
                         <label htmlFor='usuario' className='label'>Agregar Familia</label>
-                        <input
+                        <Etiqueta
                             type='text'
                             name='familia'
                             id='familia'
@@ -30,11 +68,14 @@ const AgregarFamilia = () => {
                             className='input'
                         />
                     </div>
-                    <button as='button' type='submit' className='boton'>Guardar</button>
-                </form>
+                    <div>
+
+                    </div>
+                    <Boton type='submit'><FaIcons.FaPlusSquare/></Boton>
+                </Formulario>
             </div>
             
-        </div >
+        </ContainerFamily >
     )
 }
 
