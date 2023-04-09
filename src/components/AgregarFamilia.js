2eb43110aec577e66dbde6bb26aada157d503c65
Andres Letelier <almlogistico@gmail.com>
2023-04-09 16:19:41 -0400

    actualiza rama sidebar con master y alertas

--- src/components/AgregarFamilia.js
+++ src/components/AgregarFamilia.js
@@ -1,56 +1,17 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import '../styles/agregarFamilia.css';
 import Alertas from './Alertas';
-=======
-import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import '../styles/agregarFamilia.css'
-import ListaFamilias from './ListaFamilias';
->>>>>>> master
-
-
-<<<<<<< HEAD
-    const [inputFamily, setInputFamily] = useState('')
-    const [estadoAlerta, cambiarEstadoAlerta] = useState(false);
-    const [alerta, cambiarAlerta] = useState({});
-
-
-    const handleChange = (e) => {
-        setInputFamily(e.target.value);
-       /*  if (e.target.name === 'familia') {
+import ListaFamilias from './ListaFamilias';     
+          
             
-        } else {
-            alert('Esta Familia ya fue ingresada');
-        } */
-    }
-    
-    const handleSubmit = (e)=>{
-        e.preventDefault();
-        cambiarEstadoAlerta(false);
-        cambiarAlerta({});
-
-        console.log(inputFamily)
-        if (inputFamily === 'familia') {
-          /*   setInputFamily(e.target.value); */
-            cambiarEstadoAlerta(true);
-            cambiarAlerta({
-                tipo: 'exito',
-                mensaje: 'Familia Ingresada Correctamente'
-            })
-            e.target.value = 0;
-        } else {
-            cambiarEstadoAlerta(true);
-            cambiarAlerta({
-                tipo: 'error',
-                mensaje: 'Por favor Ingrese una familia¡¡'
-            })
-            
-=======
 const AgregarFamilia = () => {
 
+    const [estadoAlerta, cambiarEstadoAlerta] = useState(false);
+    const [alerta, cambiarAlerta] = useState({});
     const [familias, setFamilias] = useState([
         // Ejemplos
         {
@@ -60,7 +21,6 @@ const AgregarFamilia = () => {
         {
             id: 2,
             texto: 'Mascarilla'
->>>>>>> master
         }
     ]);
 
@@ -73,6 +33,10 @@ const AgregarFamilia = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
 
+        cambiarEstadoAlerta(false);
+        cambiarAlerta({});
+        e.target.value="";
+
         setFamilias(
             [
                 ...familias,
@@ -82,19 +46,22 @@ const AgregarFamilia = () => {
                 }
             ]
         );
-    }
 
-    console.log(familias)
+        cambiarEstadoAlerta(true);
+            cambiarAlerta({
+                tipo: 'exito',
+                mensaje: 'Familia Ingresada Correctamente'
+            })       
+
+        
+    }
+    
 
     return (
         <div className='containerFamily'>
             <h2 className='titleForm'>Familias de Equipos</h2>
             <div>
-<<<<<<< HEAD
-                <form onSubmit={handleSubmit} className='formulario'>
-=======
                 <form action='' className='formFamily' onSubmit={handleSubmit}>
->>>>>>> master
                     <div>
                         <label htmlFor='familia' className='formFamily__label'>Agregar Familia</label>
                         <input
@@ -110,15 +77,13 @@ const AgregarFamilia = () => {
                     </button>
                 </form>
             </div>
-<<<<<<< HEAD
-            <Alertas tipo={alerta.tipo}
+            
+        <ListaFamilias familias={familias} setFamilias={setFamilias} />
+        <Alertas tipo={alerta.tipo}
                      mensaje={alerta.mensaje}
                      estadoAlerta={estadoAlerta}
                      cambiarEstadoAlerta={cambiarEstadoAlerta}
-            />
-=======
-        <ListaFamilias familias={familias} setFamilias={setFamilias} />
->>>>>>> master
+         />
         </div >
         
     )
