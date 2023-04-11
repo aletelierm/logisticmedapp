cc72f6f2c2f38483a49d9adc6b6e9ad767c76fd0
Andres Letelier <almlogistico@gmail.com>
2023-04-11 15:27:43 -0400

    rutas

---
+++ src/components/Configuracion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Configuracion = () => {
+    return (
+        <div>
+            <h1>Dashboard Configuracion</h1>
+        </div>
+    );
+};
+
+export default Configuracion;
