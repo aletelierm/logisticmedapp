b4e97bf54a455a7d3aeae9c49c563ed3de30363e
Andres Letelier <almlogistico@gmail.com>
2023-04-20 16:32:06 -0400

    actualiza menu jerarquico y rutas

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,8 +25,8 @@ import Mantenimiento from './Mantenimiento';
 import Configuracion from './Configuracion';
 import NavbarSesion from './NavbarSesion';
 import AgregarEmpresa from './AgregarEmpresa';
-import ActualizaProveedor from './ActualizaProveedor'
-
+import ActualizaProveedor from './ActualizaProveedor';
+import AsignarRoles from './AsignarRoles';
 
   // ingresando al home => erivas
 export const Home = () => {
@@ -56,6 +56,7 @@ export const Home = () => {
             <Route path="/actualiza" element={<ActualizaProveedor/>}/>
             <Route path="/configuracion" element={<Configuracion/>}/>        
             <Route path="/configuracion/registrausuarios" element={<Usuarios/>}/>
+            <Route path="/configuracion/asignaroles" element={<AsignarRoles/>}/>
             <Route path="/configuracion/agregarempresa" element={<AgregarEmpresa/>}/>
 
     </Routes>
