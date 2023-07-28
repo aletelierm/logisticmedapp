1a916bd2df46e61ccb4a7e6540dbcab4087ca207
Andres Letelier <almlogistico@gmail.com>
2023-07-28 16:57:24 -0400

    logos navbar

--- src/components/NavbarSesion.js
+++ src/components/NavbarSesion.js
@@ -21,7 +21,7 @@ const NavbarSesion = () => {
         <>
             <div className='navbar'>
                 <div className='imageLogo'>
-                    <img src='../../logo.png' alt='Logo' />
+                    <img src='../../logo.png' alt='Logo' style={{height:'240px'}}/>
                 </div>
                 <div>
                     v1.0 Beta
