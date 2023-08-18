813410f6550994c3a538e32fc98c3f122e60c911
Catalina Astudillo <Catalina.astudillo7@gmail.com>
2023-08-18 17:27:11 -0400

    grabar cabecera y detalles de equipos no entregados

--- src/firebase/EntradasDB.js
+++ src/firebase/EntradasDB.js
@@ -2,7 +2,7 @@
 import { db } from './firebaseConfig';
 import { collection, addDoc } from "firebase/firestore";
 
-const EntradasDB = async ({ tipDoc, numDoc, date, tipoInOut, rut, entidad, eq_id, familia, tipo, marca, modelo, serie, rfid, price, cab_id, tipMov, userAdd, userMod, fechaAdd, fechaMod, emp_id }) => {
+const EntradasDB = async ({ tipDoc, numDoc, date, tipoInOut, rut, entidad, eq_id, familia, tipo, marca, modelo, serie, rfid, price, cab_id, tipMov, userAdd, userMod, fechaAdd, fechaMod, emp_id, observacion }) => {
 
     return await addDoc(collection(db, 'entradas'), {
         tipdoc: tipDoc,
@@ -25,7 +25,8 @@ const EntradasDB = async ({ tipDoc, numDoc, date, tipoInOut, rut, entidad, eq_id
         usermod: userMod,
         fechaadd: fechaAdd,
         fechamod: fechaMod,
-        emp_id: emp_id
+        emp_id: emp_id,
+        observacion: observacion
     })
 }
 
