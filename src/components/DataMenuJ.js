fb817ef476aef2c4015b8d8d14494f50f42bd317
Andres Letelier <almlogistico@gmail.com>
2023-09-29 12:37:32 -0300

    corrige perfil jdamin

--- src/components/DataMenuJ.js
+++ src/components/DataMenuJ.js
@@ -7,41 +7,7 @@ import * as MdIcons from 'react-icons/md';
 
 export const DataMenuJadmin = [
 
-  {
-    title: 'Mantenciones',
-    path: 'serviciotecnico',
-    icon: <MdIcons.MdMedicalServices />,
-    iconClosed: <RiIcons.RiArrowDownSFill />,
-    iconOpened: <RiIcons.RiArrowUpSFill />,
-    subNav: [
-      {
-        title: 'Items',
-        path: 'serviciotecnico/items',
-        icon: <IoIcons.IoIosAlbums />
-      },
-      {
-        title: 'Protocolos',
-        path: 'serviciotecnico/protocolos',
-        icon: <IoIcons.IoIosAlbums />
-      },
-      {
-        title: 'Programas',
-        path: 'serviciotecnico/programas',
-        icon: <IoIcons.IoIosTimer />
-      },
-      {
-        title: 'Reportes',
-        path: 'serviciotecnico/certificados',
-        icon: <FaIcons.FaCertificate />
-      },
-      {
-        title: 'Ejecutar Mant.',
-        path: 'serviciotecnico/mantencion',
-        icon: <IoIcons.IoIosPlay />
-      }
-    ]
-  },
-  {
+   {
     title: 'Configuracion',
     path: 'configuracion',
     icon: <MdIcons.MdManageAccounts />,
