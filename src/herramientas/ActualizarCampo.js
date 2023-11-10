fc27bd3c782d13efa517682686c52c85e53e0647
Andres Letelier <almlogistico@gmail.com>
2023-11-10 16:38:14 -0300

    herra

--- src/herramientas/ActualizarCampo.js
+++ src/herramientas/ActualizarCampo.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../../../herramientasScript/logisticmedappdesa-firebase-adminsdk-9tt67-94cb85303c.json");
+var serviceAccount = require("../../herramientasScript/logisticmedappdesa-firebase-adminsdk-9tt67-94cb85303c.json");
 /* var serviceAccount = require(process.env.SERVICE_ACCOUNT); */
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
