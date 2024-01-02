cd10aba4483ae0a910066fcf4e36f876afff5791
Catalina Astudillo <Catalina.astudillo7@gmail.com>
2024-01-02 19:02:49 -0300

    actualizado

--- src/components/EjecutarMantencion.js
+++ src/components/EjecutarMantencion.js
@@ -68,7 +68,7 @@ const EjecutarMantencion = () => {
         setItemsInst(documenInst);
         const docCheck = query(collection(db, 'protocolos'), where('emp_id', '==', users.emp_id), where('cab_id', '==', protocoloCab.current), where('categoria', '==', 'CHECK'));
         const docuCheck = await getDocs(docCheck);
-        const documenCheck = (docuCheck.docs.map((doc) => ({ ...doc.data(), id: doc.id, valor: false})));
+        const documenCheck = (docuCheck.docs.map((doc) => ({ ...doc.data(), id: doc.id, valor: false })));
         setItemsCheck(documenCheck);
         const docLlen = query(collection(db, 'protocolos'), where('emp_id', '==', users.emp_id), where('cab_id', '==', protocoloCab.current), where('categoria', '==', 'LLENADO'));
         const docuLlen = await getDocs(docLlen);
@@ -79,150 +79,149 @@ const EjecutarMantencion = () => {
         const documenSel = (docuSel.docs.map((doc) => ({ ...doc.data(), id: doc.id, pasa: false, nopasa: false })));
         setItemsSelec(documenSel);
     }
-    console.log('itemsCheck', itemsCheck)
 
     const handleButtonClick = (index, buttonId) => {
         setItemsCheck((prevItems) => {
             const nuevosElementos = [...prevItems];
-        console.log('nuevosElementos', nuevosElementos)
+            console.log('nuevosElementos', nuevosElementos)
             nuevosElementos[index].valor = buttonId;
             return nuevosElementos;
         });
     }
 
-// const handleSubmit = (e) => {
-//     e.preventDefault();
-//     cambiarEstadoAlerta(false);
-//     cambiarAlerta({});        
-// }
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     cambiarEstadoAlerta(false);
+    //     cambiarAlerta({});        
+    // }
 
-useEffect(() => {
-    consultarProtocolos();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-}, [manto, navigate])
+    useEffect(() => {
+        consultarProtocolos();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [manto, navigate])
 
-return (
-    <ContenedorCliente style={{ width: '800px' }}>
-        <Contenedor>
-            <Titulo>Ejecutar Mantención</Titulo>
-        </Contenedor>
-        <Contenedor>
-            <Titulo>{nombreProtocolo}</Titulo>
-            <ContentElemenMov>
-                <ContentElemenSelect style={{ paddingBottom: '5px' }}>
-                    <Label>Familia :</Label>
-                    <Label style={{ fontSize: '15px' }}>{familia}</Label>
-                </ContentElemenSelect>
-                <ContentElemenSelect style={{ paddingBottom: '5px' }}>
-                    <Label>Tipo Equipamiento :</Label>
-                    <Label style={{ fontSize: '15px' }}>{tipo}</Label>
-                </ContentElemenSelect>
-            </ContentElemenMov>
-            <ContentElemenMov>
-                <ContentElemenSelect style={{ padding: '5px' }}>
-                    <Label>Instrumentos requeridos :</Label>
-                    <ul >
-                        {itemsInst.map((item, index) => {
-                            return (
-                                <li style={{ fontSize: '12px' }} key={index}>{item.item}</li>
-                            )
-                        })}
-                    </ul>
-                </ContentElemenSelect>
-            </ContentElemenMov>
-            <BotonGuardar>Guardar</BotonGuardar>
-
-            <Table singleLine>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>N°</Table.HeaderCell>
-                        <Table.HeaderCell>Item</Table.HeaderCell>
-                        <Table.HeaderCell></Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {itemsCheck.map((item, index) => {
-                        return (
-                            <Table.Row key={index}>
-                                <Table.Cell >{index + 1}</Table.Cell>
-                                <Table.Cell style={{ whiteSpace: 'normal', wordWrap: 'break-word', fontSize: '12px' }}>{item.item}</Table.Cell>
-                                <Table.Cell style={{ textAlign: 'center' }}>
-                                    <BotonCheck
-                                        onClick={() => handleButtonClick(index, 'pasa')}
-                                        className={item.valor === 'pasa' ? 'activeBoton' : ''}
-                                    >Pasa</BotonCheck>
-                                    <BotonCheck
-                                        onClick={() => handleButtonClick(index, 'nopasa')}
-                                        className={item.valor === 'nopasa' ? 'activeBoton' : ''}
-                                    >No Pasa</BotonCheck>
-                                    <BotonCheck
-                                        onClick={() => handleButtonClick(index, 'na')}
-                                        className={item.valor === 'na' ? 'activeBoton' : ''}
-                                    >N/A</BotonCheck>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
-            </Table>
-
-            <Titulo style={{ fontSize: '18px' }}>Tabla de vacio</Titulo>
-            <Table singleLine>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>N°</Table.HeaderCell>
-                        <Table.HeaderCell>Item</Table.HeaderCell>
-                        <Table.HeaderCell>Referencia</Table.HeaderCell>
-                        <Table.HeaderCell style={{ textAlign: 'center' }}>Pasa</Table.HeaderCell>
-                        <Table.HeaderCell style={{ textAlign: 'center' }}>No Pasa</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {itemsLlenado.map((item, index) => {
-                        return (
-                            <Table.Row key={index} >
-                                <Table.Cell >{index + 1}</Table.Cell>
-                                <Table.Cell style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>{item.item}</Table.Cell>
-                                <Table.Cell  ><Input type="text" /></Table.Cell>
-                                <Table.Cell style={{ textAlign: 'center' }}><Input type="checkbox" /></Table.Cell>
-                                <Table.Cell style={{ textAlign: 'center' }}><Input type="checkbox" /></Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
-            </Table>
-
-            <Titulo style={{ fontSize: '18px' }}>Seguridad electrica</Titulo>
-            <ContentElemenMov>
-
-                {itemsSelec.map((item, index) => {
-                    return (
-                        <Select key={index}>
-                            <option key={index}>{item.item} :</option>
-                            {Opcion.map((o, index) => {
+    return (
+        <ContenedorCliente style={{ width: '800px' }}>
+            <Contenedor>
+                <Titulo>Ejecutar Mantención</Titulo>
+            </Contenedor>
+            <Contenedor>
+                <Titulo>{nombreProtocolo}</Titulo>
+                <ContentElemenMov>
+                    <ContentElemenSelect style={{ paddingBottom: '5px' }}>
+                        <Label>Familia :</Label>
+                        <Label style={{ fontSize: '15px' }}>{familia}</Label>
+                    </ContentElemenSelect>
+                    <ContentElemenSelect style={{ paddingBottom: '5px' }}>
+                        <Label>Tipo Equipamiento :</Label>
+                        <Label style={{ fontSize: '15px' }}>{tipo}</Label>
+                    </ContentElemenSelect>
+                </ContentElemenMov>
+                <ContentElemenMov>
+                    <ContentElemenSelect style={{ padding: '5px' }}>
+                        <Label>Instrumentos requeridos :</Label>
+                        <ul >
+                            {itemsInst.map((item, index) => {
                                 return (
-                                    <option key={index}>{o.text}</option>
+                                    <li style={{ fontSize: '12px' }} key={index}>{item.item}</li>
                                 )
                             })}
-                        </Select>
-                    )
-                })}
-            </ContentElemenMov>
-
-            <ContentElemenMov style={{ marginTop: '10px' }}>
+                        </ul>
+                    </ContentElemenSelect>
+                </ContentElemenMov>
                 <BotonGuardar>Guardar</BotonGuardar>
-                <BotonGuardar>Confirmar</BotonGuardar>
-            </ContentElemenMov>
-        </Contenedor>
-        <BotonGuardar style={{ marginTop: '30px' }} onClick={volver} >Volver</BotonGuardar>
-        <Alerta
-            tipo={alerta.tipo}
-            mensaje={alerta.mensaje}
-            estadoAlerta={estadoAlerta}
-            cambiarEstadoAlerta={cambiarEstadoAlerta}
-        />
-    </ContenedorCliente>
-)
+
+                <Table singleLine>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>N°</Table.HeaderCell>
+                            <Table.HeaderCell>Item</Table.HeaderCell>
+                            <Table.HeaderCell></Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {itemsCheck.map((item, index) => {
+                            return (
+                                <Table.Row key={index}>
+                                    <Table.Cell >{index + 1}</Table.Cell>
+                                    <Table.Cell style={{ whiteSpace: 'normal', wordWrap: 'break-word', fontSize: '12px' }}>{item.item}</Table.Cell>
+                                    <Table.Cell style={{ textAlign: 'center' }}>
+                                        <BotonCheck
+                                            onClick={() => handleButtonClick(index, 'pasa')}
+                                            className={item.valor === 'pasa' ? 'activeBoton' : ''}
+                                        >Pasa</BotonCheck>
+                                        <BotonCheck
+                                            onClick={() => handleButtonClick(index, 'nopasa')}
+                                            className={item.valor === 'nopasa' ? 'activeBoton' : ''}
+                                        >No Pasa</BotonCheck>
+                                        <BotonCheck
+                                            onClick={() => handleButtonClick(index, 'na')}
+                                            className={item.valor === 'na' ? 'activeBoton' : ''}
+                                        >N/A</BotonCheck>
+                                    </Table.Cell>
+                                </Table.Row>
+                            )
+                        })}
+                    </Table.Body>
+                </Table>
+
+                <Titulo style={{ fontSize: '18px' }}>Tabla de vacio</Titulo>
+                <Table singleLine>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>N°</Table.HeaderCell>
+                            <Table.HeaderCell>Item</Table.HeaderCell>
+                            <Table.HeaderCell>Referencia</Table.HeaderCell>
+                            <Table.HeaderCell style={{ textAlign: 'center' }}>Pasa</Table.HeaderCell>
+                            <Table.HeaderCell style={{ textAlign: 'center' }}>No Pasa</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {itemsLlenado.map((item, index) => {
+                            return (
+                                <Table.Row key={index} >
+                                    <Table.Cell >{index + 1}</Table.Cell>
+                                    <Table.Cell style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>{item.item}</Table.Cell>
+                                    <Table.Cell  ><Input type="text" /></Table.Cell>
+                                    <Table.Cell style={{ textAlign: 'center' }}><Input type="checkbox" /></Table.Cell>
+                                    <Table.Cell style={{ textAlign: 'center' }}><Input type="checkbox" /></Table.Cell>
+                                </Table.Row>
+                            )
+                        })}
+                    </Table.Body>
+                </Table>
+
+                <Titulo style={{ fontSize: '18px' }}>Seguridad electrica</Titulo>
+                <ContentElemenMov>
+
+                    {itemsSelec.map((item, index) => {
+                        return (
+                            <Select key={index}>
+                                <option key={index}>{item.item} :</option>
+                                {Opcion.map((o, index) => {
+                                    return (
+                                        <option key={index}>{o.text}</option>
+                                    )
+                                })}
+                            </Select>
+                        )
+                    })}
+                </ContentElemenMov>
+
+                <ContentElemenMov style={{ marginTop: '10px' }}>
+                    <BotonGuardar>Guardar</BotonGuardar>
+                    <BotonGuardar>Confirmar</BotonGuardar>
+                </ContentElemenMov>
+            </Contenedor>
+            <BotonGuardar style={{ marginTop: '30px' }} onClick={volver} >Volver</BotonGuardar>
+            <Alerta
+                tipo={alerta.tipo}
+                mensaje={alerta.mensaje}
+                estadoAlerta={estadoAlerta}
+                cambiarEstadoAlerta={cambiarEstadoAlerta}
+            />
+        </ContenedorCliente>
+    )
 }
 
 export default EjecutarMantencion;
